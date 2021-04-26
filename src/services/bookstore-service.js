926d3700e73d7ae23439a70d1c634595140e8caa
Oleh Bohdan <sentance22@gmail.com>
2021-04-26 20:30:05 +0200

    added data to getBooks

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,8 @@
 export default class BookStoreService {
     getBooks(){
-        return[]
+        return[
+            {id: 1, name: "Prod to Linux", author: "Susana"},
+            {id: 2, name: "Prod to Microsoft", author: "Kate"},
+        ]
     }
 }
